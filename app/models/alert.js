866eec6fa8025ff04e37321564dd593c3a16fce9
SergioGutzB <sergut18@gmail.com>
2016-05-11 14:26:47 -0500

    add data share alert

--- app/models/alert.js
+++ app/models/alert.js
@@ -7,6 +7,8 @@ var AlertSchema = new Schema({
   user_id: {type: String},
   message: [],
   create: {type: Date, default: Date.now}
+  data_user: false,
+  data_sender: false,
 });
 
 module.exports = mongoose.model('Alert', AlertSchema);
